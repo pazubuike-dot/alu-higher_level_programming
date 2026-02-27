bf3ef17e24e0dfd1f403302d93fb02a680643b41
pazubuike-dot <p.azubuike@alustudent.com>
2026-02-27 14:00:19 +0200

    another fixing

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-word2 = "is fun!"
-print(word[:26] + word[30:])
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+str = (str[39:66] + str[-23:-17] + str[0:6])
+print(str)
--- python-hello_world/README.md
+++ python-hello_world/README.md
@@ -1,1 +1,1 @@
-Hello python.
+nice
