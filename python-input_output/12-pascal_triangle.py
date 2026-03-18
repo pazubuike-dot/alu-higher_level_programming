184d7b9b0da5e7aea5532a4aed8959485bea7f82
pazubuike-dot <p.azubuike@alustudent.com>
2026-03-18 15:11:02 +0200

    Add files and set executable permissions

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        last_row = triangle[-1]
+        new_row = [1]
+        for i in range(len(last_row) - 1):
+            new_row.append(last_row[i] + last_row[i + 1])
+        new_row.append(1)
+        triangle.append(new_row)
+    return triangle
