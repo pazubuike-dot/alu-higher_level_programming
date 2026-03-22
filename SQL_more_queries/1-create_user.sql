a3f59df9c09ea042df376e301a9871880081b075
pazubuike-dot <p.azubuike@alustudent.com>
2026-03-22 05:20:08 -0700

    Fixing tasks 0 and 1 formatting and logic

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- Creates user_0d_1 with all privileges and specific password
+-- Creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
 
