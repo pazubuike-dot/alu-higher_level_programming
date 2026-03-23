86fe26be99332a4fe9146a296fa6bfa1e2fd641c
pazubuike-dot <p.azubuike@alustudent.com>
2026-03-23 16:02:04 -0700

    Use BINARY for case-sensitive filtering

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
+"""
+Lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Connect to the MySQL database
-    # sys.argv contains: [0]script_name, [1]user, [2]password, [3]db_name
+    # Connect to a MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,8 +20,9 @@ if __name__ == "__main__":
     # Create a cursor object to execute queries
     cursor = db.cursor()
 
-    # Execute the SQL query to get all states sorted by id
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    # Execute the SQL query with BINARY to force case sensitivity
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                   ORDER BY id ASC")
 
     # Fetch all the rows from the executed query
     query_rows = cursor.fetchall()
