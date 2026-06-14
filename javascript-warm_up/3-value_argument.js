94b1c4454ae0a5f387f546b4961bd23a748b94f0
pazubuike-dot <p.azubuike@alustudent.com>
2026-06-14 21:45:02 +0200

    Standardize syntax and line endings

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+@#\!/usr/bin/node
 const firstArg = process.argv[2];
 
 if (firstArg === undefined) {
--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
-#!/usr/bin/node
+@#\!/usr/bin/node
 const parsedInt = parseInt(process.argv[2], 10);
 
 if (isNaN(parsedInt)) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${parsedInt}`);
+  console.log(\My number: \\);
 }
